b72fc90b15ae5d14765cb1748bba8fc7bdd0296e
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2024-11-06 14:37:58 -0600

    initial commit with uid changes already applied

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const packageJson = require("./package.json");
+
+module.exports = {
+  name: packageJson.name,
+  displayName: packageJson.name,
+  rootDir: "./",
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.test.json",
+    },
+  },
+  preset: "ts-jest",
+  testMatch: ["**/src/__tests__/**/*.test.ts"],
+  testEnvironment: "node",
+  testTimeout: 180000,
+  collectCoverage: true,
+  moduleNameMapper: {
+    "firebase-admin/firestore":
+      "<rootDir>/node_modules/firebase-admin/lib/firestore",
+  },
+};
